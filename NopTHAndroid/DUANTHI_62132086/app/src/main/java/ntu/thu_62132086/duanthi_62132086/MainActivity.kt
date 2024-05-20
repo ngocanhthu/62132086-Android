5f11a2aa08ee2e95ac38866b23afb576ba8d919a
thunguyenngocanh <121885372+ngocanhthu@users.noreply.github.com>
2024-05-20 15:53:07 +0700

    duanthi

---
+++ NopTHAndroid/DUANTHI_62132086/app/src/main/java/ntu/thu_62132086/duanthi_62132086/MainActivity.kt
@@ -0,0 +1,11 @@
+package ntu.thu_62132086.duanthi_62132086
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ NopTHAndroid/DUANTHI_62132086/app/src/main/java/ntu/thu_62132086/duanthi_62132086/QuizOption.java
@@ -0,0 +1,4 @@
+package ntu.thu_62132086.duanthi_62132086;
+
+public class QuizOption {
+}
